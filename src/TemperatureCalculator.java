470d43d3eadedffd61157823d1e193b66d5ed7a0
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-03-06 12:35:05 -0500

    Comments out basic outline

--- src/TemperatureCalculator.java
+++ src/TemperatureCalculator.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class TemperatureCalculator {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
     }
 }
+
+
+// F to C Formula: 
+    // (Ftemp - 32) * (5/9)
+
+// C to F Formula:
+    //  (Ctemp * 1.8) + 32
+
+
+
+    // Prompt user to choose if they are converting Fahrenheit to Celsius OR Celsius to Fahrenheit
+    // When they have chosen enter an if/else statement:
+        // If (F to C){
+            // confirm for the user the choice via output
+            // Prompt user to input their Fahrenheit Temperature (fTemp)
+            // run the temperature they have given through the F to C formula
+                // cTemp = (fTemp - 32)/(5/9)
+        // } else if (C to F){
+            // confirm to the user their choice via output
+            // Prompt user to input their Celsius Temperature (cTemp)
+            // run the temperature they have given through the C to F formula
+                //  fTemp = (cTemp * 1.8) + 32
+        // }
