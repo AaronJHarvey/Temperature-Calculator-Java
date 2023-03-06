8382c972de651a0d6d712b177987c58bc1febd67
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-03-06 12:36:23 -0500

    Declares variables

--- src/TemperatureCalculator.java
+++ src/TemperatureCalculator.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class TemperatureCalculator {
     public static void main(String[] args) {
+        private double cTemp;
+        private int fTemp;
         
     }
 }
